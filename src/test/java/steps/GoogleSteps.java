ddecae2bc96948456d51352a9376042b67b7666f
xristi <xristix@ymail.com>
2018-09-28 11:19:42 +0300

    context config added - to be reviewed

--- src/test/java/steps/GoogleSteps.java
+++ src/test/java/steps/GoogleSteps.java
@@ -5,12 +5,17 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import pages.GooglePage;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by Xristi on 9/20/2018.
  */
+@ContextConfiguration(classes = {Config.class})
 public class GoogleSteps {
 
+    @Autowired
+    private Properties properties;
     private GooglePage googlePage;
 
     @Given("^the google page is opened$")
